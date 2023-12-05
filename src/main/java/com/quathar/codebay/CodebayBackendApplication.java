97524eff1f8b5c8970024434ddfa084adaa6acc9
Q <99633210+Quathar@users.noreply.github.com>
2023-12-05 21:19:45 +0100

    Initial Commit

---
+++ src/main/java/com/quathar/codebay/CodebayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quathar.codebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quathar/codebay/CodebayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quathar.codebay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
