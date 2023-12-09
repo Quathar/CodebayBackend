a7354ec2a4d2d7ad8bc169074e7637c47b7b3bc2
Q <99633210+Quathar@users.noreply.github.com>
2023-12-09 17:50:01 +0100

    Create main models

---
+++ domain/src/main/java/com/quathar/codebay/domain/model/Catalog.java
@@ -0,0 +1,19 @@
+package com.quathar.codebay.domain.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <h1>Catalog Model</h1>
+ *
+ * @since 2023-05-13
+ * @version 1.0
+ * @author Q
+ */
+@Builder
+@Getter
+@Setter
+public class Catalog {
+
+}
---
+++ domain/src/main/java/com/quathar/codebay/domain/model/Supplier.java
@@ -0,0 +1,19 @@
+package com.quathar.codebay.domain.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <h1>Supplier Model</h1>
+ *
+ * @since 2023-03-27
+ * @version 1.0
+ * @author Q
+ */
+@Builder
+@Getter
+@Setter
+public class Supplier {
+
+}
