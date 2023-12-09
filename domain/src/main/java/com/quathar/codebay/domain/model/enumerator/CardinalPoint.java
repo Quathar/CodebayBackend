ab7bd770d78529c817c558d6904bedd690ccc9d7
Q <99633210+Quathar@users.noreply.github.com>
2023-12-09 22:25:34 +0100

    Add essential enums

---
+++ domain/src/main/java/com/quathar/codebay/domain/model/enumerator/CardinalPoint.java
@@ -0,0 +1,17 @@
+package com.quathar.codebay.domain.model.enumerator;
+
+/**
+ * <h1>Cardinal Point</h1>
+ *
+ * @since 2023-12-09
+ * @version 1.0
+ * @author Q
+ */
+public enum CardinalPoint {
+
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+
+}
