e5adc8013a4c40cde7be7e3245a08dc658574c0f
Q <99633210+Quathar@users.noreply.github.com>
2023-12-09 18:53:50 +0100

    Create ResourceNotFoundException

---
+++ domain/src/main/java/com/quathar/codebay/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.quathar.codebay.domain.exception;
+
+/**
+ * <h1>Resource Not Found Exception</h1>
+ *
+ * @since 2023-03-30
+ * @version 1.0
+ * @author Q
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    // <<-CONSTRUCTOR->>
+    public ResourceNotFoundException() {
+        super();
+    }
+
+}
