c2c44a4898768c9b37fda5fd6dad3d2d654a97a8
Q <99633210+Quathar@users.noreply.github.com>
2023-12-14 10:19:04 +0100

    Update modules build.gradle.kts

--- bootloader/build.gradle.kts
+++ bootloader/build.gradle.kts
@@ -1,3 +1,8 @@
+
 dependencies {
+    // [ Spring Boot ]
     implementation("org.springframework.boot:spring-boot-starter")
+
+    // [ H2 Database ]
+    runtimeOnly("com.h2database:h2")
 }
--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,8 +1,14 @@
+
+val mapstructVersion = "1.5.5.Final"
+
 dependencies {
-    // [ Spring Boot ]
+    // [ General ]
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
     // [ Input Adapters Dependencies ]
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // [ Output Adapters Dependencies ]
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
