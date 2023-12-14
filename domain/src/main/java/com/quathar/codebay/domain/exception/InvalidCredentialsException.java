4c70fe1c79cbd7b33a1c6312b44dcdd56b66ae62
Q <99633210+Quathar@users.noreply.github.com>
2023-12-14 19:32:27 +0100

    Create Invalid Credentials Exception

---
+++ domain/src/main/java/com/quathar/codebay/domain/exception/InvalidCredentialsException.java
@@ -0,0 +1,17 @@
+package com.quathar.codebay.domain.exception;
+
+/**
+ * <h1>Invalid Credentials Exception</h1>
+ *
+ * @since 2023-12-14
+ * @version 1.0
+ * @author Q
+ */
+public class InvalidCredentialsException extends RuntimeException {
+
+    // <<-CONSTRUCTOR->>
+    public InvalidCredentialsException() {
+        super();
+    }
+
+}
