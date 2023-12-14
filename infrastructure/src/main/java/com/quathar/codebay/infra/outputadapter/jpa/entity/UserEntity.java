90633b6ffe0ad966f23fe61729544351832b396b
Q <99633210+Quathar@users.noreply.github.com>
2023-12-14 19:33:09 +0100

    Update the user entity fields

--- infrastructure/src/main/java/com/quathar/codebay/infra/outputadapter/jpa/entity/UserEntity.java
+++ infrastructure/src/main/java/com/quathar/codebay/infra/outputadapter/jpa/entity/UserEntity.java
@@ -1,5 +1,7 @@
 package com.quathar.codebay.infra.outputadapter.jpa.entity;
 
+import com.quathar.codebay.domain.model.enumerator.UserStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -30,15 +33,32 @@ public class UserEntity {
     // <<-FIELDS->>
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID   id;
+    private UUID id;
+
     @Column(nullable = false, unique = true)
     private String username;
+
     @Column(nullable = false)
     private String password;
+
     @Column(nullable = false)
     private String nickname;
+
     @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private Integer successfulAuth;
+
+    @Column(nullable = false)
+    private Integer failedAuth;
+
+    private LocalDateTime lastConnection;
+
+    private LocalDateTime releaseBlock;
+
+    @Column(nullable = false)
+    private UserStatus status;
 
     // TODO: Add all the fields necessary for persistence
 
