89eab61fe43b65c49b40609f6370be97067e21f1
Q <99633210+Quathar@users.noreply.github.com>
2023-12-14 19:32:02 +0100

    Set the user sign up process and implement the log-in use case

--- application/src/main/java/com/quathar/codebay/application/service/UserService.java
+++ application/src/main/java/com/quathar/codebay/application/service/UserService.java
@@ -2,8 +2,13 @@ package com.quathar.codebay.application.service;
 
 import com.quathar.codebay.application.inputport.UserServicePort;
 import com.quathar.codebay.application.outputport.UserRepositoryPort;
+import com.quathar.codebay.application.util.PasswordHasher;
+import com.quathar.codebay.domain.exception.InvalidCredentialsException;
+import com.quathar.codebay.domain.exception.ResourceNotFoundException;
 import com.quathar.codebay.domain.model.User;
+import com.quathar.codebay.domain.model.enumerator.UserStatus;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -22,6 +27,37 @@ public class UserService extends CrudService<User, UUID> implements UserServiceP
     public UserService(UserRepositoryPort userRepositoryPort) {
         super(userRepositoryPort);
         this.userRepositoryPort = userRepositoryPort;
+    }
+
+    // <<-METHODS->>
+    @Override
+    public User create(User user) {
+        String password = user.getPassword();
+        user.setPassword(PasswordHasher.hash(password));
+        user.setStatus(UserStatus.ACTIVE);
+        user.setSuccessfulAuth(0);
+        user.setFailedAuth(0);
+        return this.userRepositoryPort.save(user);
+    }
+
+    @Override
+    public String login(String username, String password) {
+        User user = this.userRepositoryPort.findByUsername(username).orElseThrow(InvalidCredentialsException::new);
+
+        if (!PasswordHasher.matches(password, user.getPassword())) {
+            Integer failedAuth = user.getFailedAuth();
+            user.setFailedAuth(++failedAuth);
+            throw new InvalidCredentialsException();
+        }
+
+        Integer successfulAuth = user.getSuccessfulAuth();
+        user.setSuccessfulAuth(++successfulAuth);
+        user.setLastConnection(LocalDateTime.now());
+
+        // TODO: finish auth
+        // Generate token
+        // Save token in the repository
+        return "token";
     }
 
 }
