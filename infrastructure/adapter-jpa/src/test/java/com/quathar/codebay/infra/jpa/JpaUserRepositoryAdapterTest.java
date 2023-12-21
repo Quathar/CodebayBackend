d2565230b826672b4c7ac88dbca1ad9045f39f25
Q <99633210+Quathar@users.noreply.github.com>
2023-12-21 18:34:02 +0100

    Create test structure

---
+++ infrastructure/adapter-jpa/src/test/java/com/quathar/codebay/infra/jpa/JpaUserRepositoryAdapterTest.java
@@ -0,0 +1,41 @@
+package com.quathar.codebay.infra.jpa;
+
+import com.quathar.codebay.infra.jpa.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * <h1>JPA User Repository Adapter Test</h1>
+ *
+ * @since 2023-12-21
+ * @version 1.0
+ * @author Q
+ */
+@DataJpaTest
+@ComponentScan({"com.quathar.codebay.infra.jpa"})
+@ContextConfiguration(classes = {JpaUserRepositoryAdapter.class})
+class JpaUserRepositoryAdapterTest {
+
+    // <<-FIELD->>
+    private JpaUserRepositoryAdapter jpaUserRepositoryAdapter;
+    private UserMapper userMapper;
+
+    // <<-CONSTRUCTOR->>
+    @Autowired
+    JpaUserRepositoryAdapterTest(JpaUserRepositoryAdapter jpaUserRepositoryAdapter) {
+        this.jpaUserRepositoryAdapter = jpaUserRepositoryAdapter;
+        this.userMapper               = UserMapper.getInstance();
+    }
+
+    // <<-TESTS->>
+    @Test
+    void ss() {
+        // TODO: Implement this
+        // TODO: Implement this
+        // TODO: Implement this
+    }
+
+}
