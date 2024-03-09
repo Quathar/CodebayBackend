ff619bc243a1fa0bcf8b22cc2141af4cf76d5cea
Q <99633210+Quathar@users.noreply.github.com>
2024-03-09 17:04:09 +0100

    Update adapter-rest build.gradle.kts

--- infrastructure/adapter-rest/build.gradle.kts
+++ infrastructure/adapter-rest/build.gradle.kts
@@ -1,6 +1,33 @@
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-dependencies {
-    // [ Spring Boot ]
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    dependencies {
+        // [ Spring Boot ]
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+    }
 }
+
+// <<-PROJECTS CONFIGURATION->>
+listOf("general", "security", "shop", "management")
+    .forEach { projectName ->
+        project(":infrastructure:adapter-rest:$projectName") {
+            tasks.getByName("bootJar") {
+                enabled = false
+            }
+
+            tasks.getByName("jar") {
+                enabled = true
+            }
+
+            dependencies {
+                implementation(project(":infrastructure:adapter-jpa"))
+                implementation(project(":infrastructure:adapter-memory"))
+
+                if (projectName != "general")
+                    implementation(project(":infrastructure:adapter-rest:general"))
+            }
+        }
+    }
