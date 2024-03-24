12aaeb1a1c288a15f994f6ff807ef5e8cdbb08ac
Q <99633210+Quathar@users.noreply.github.com>
2024-03-24 23:26:05 +0100

    Create Role & Operation JPA repositories

---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/security/JpaRoleRepository.java
@@ -0,0 +1,26 @@
+package com.quathar.codebay.infra.jpa.repository.security;
+
+import com.quathar.codebay.infra.jpa.entity.security.RoleEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <h1>JPA (Java Persistence API) Role Repository</h1>
+ *
+ * @since 2024-03-21
+ * @version 1.0
+ * @author Q
+ */
+@Repository
+public interface JpaRoleRepository extends JpaRepository<RoleEntity, java.util.UUID> {
+
+    /**
+     * Finds a role entity by its name.
+     *
+     * @param roleName The name of the role entity to find.
+     * @return An Optional containing the found role entity, or empty if not found.
+     */
+    java.util.Optional<RoleEntity> findByName(String roleName);
+
+}
