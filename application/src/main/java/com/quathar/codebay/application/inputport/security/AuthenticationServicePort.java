5575efda47b79e97ec30b02d2aba6c3ada92c359
Q <99633210+Quathar@users.noreply.github.com>
2024-03-24 22:45:13 +0100

    Create AuthenticationServicePort

---
+++ application/src/main/java/com/quathar/codebay/application/inputport/security/AuthenticationServicePort.java
@@ -0,0 +1,13 @@
+package com.quathar.codebay.application.inputport.security;
+
+import com.quathar.codebay.application.usecase.security.AuthenticationUseCase;
+
+/**
+ * <h1>Authentication Service Port</h1>
+ *
+ * @since 2024-03-12
+ * @version 1.0
+ * @author Q
+ */
+public interface AuthenticationServicePort extends AuthenticationUseCase {
+}
