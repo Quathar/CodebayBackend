b8fdb9d1c5db354c4ee6739b3a6cdecb213c21e5
Q <99633210+Quathar@users.noreply.github.com>
2024-03-25 22:18:35 +0100

    Create GrantPermissionRequest

---
+++ infrastructure/adapter-rest/security/src/main/java/com/quathar/codebay/infra/security/model/request/GrantPermissionRequest.java
@@ -0,0 +1,25 @@
+package com.quathar.codebay.infra.security.model.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * <h1>Grant Permission Request</h1>
+ *
+ * Represents a request to grant a permission.
+ *
+ * @param role      The role to which the permission will be granted.
+ * @param operation The operation for which permission will be granted.
+ *
+ * @since 2024-03-21
+ * @version 1.0
+ * @author Q
+ */
+public record GrantPermissionRequest(
+        @NotNull
+        @NotBlank
+        String role,
+        @NotNull
+        @NotBlank
+        String operation
+) {}
