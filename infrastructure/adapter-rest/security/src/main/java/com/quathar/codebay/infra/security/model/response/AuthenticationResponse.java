af181ea2ce44aa89fd3c8bbf7ff9a3320f54fecb
Q <99633210+Quathar@users.noreply.github.com>
2024-03-25 01:13:32 +0100

    Create AuthenticationResponse

---
+++ infrastructure/adapter-rest/security/src/main/java/com/quathar/codebay/infra/security/model/response/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.quathar.codebay.infra.security.model.response;
+
+/**
+ * <h1>Token Pair Model</h1>
+ *
+ * Represents a token pair used in authentication responses.
+ * This record contains the access token and refresh token included in an authentication response.
+ *
+ * @param accessToken  The access token used for authentication and authorization.
+ * @param refreshToken The refresh token used for token refreshing.
+ *
+ * @since 2023-12-27
+ * @version 1.0
+ * @author Q
+ */
+public record AuthenticationResponse(
+        String accessToken,
+        String refreshToken
+) {}
