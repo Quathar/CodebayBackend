2cf0f2e4dfe50972385ef7358ad4f7d10257be99
Q <99633210+Quathar@users.noreply.github.com>
2024-03-25 22:30:21 +0100

    Create Permission mappers

---
+++ infrastructure/adapter-rest/security/src/main/java/com/quathar/codebay/infra/security/model/mapper/GrantPermissionRequestMapper.java
@@ -0,0 +1,32 @@
+package com.quathar.codebay.infra.security.model.mapper;
+
+import com.quathar.codebay.domain.model.security.GrantedPermission;
+import com.quathar.codebay.infra.security.model.request.GrantPermissionRequest;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+/**
+ * <h1>Granted Permission Request Mapper</h1>
+ *
+ * @see GrantPermissionRequest
+ * @see HttpPermissionMapper
+ * @since 2024-03-21
+ * @version 1.0
+ * @author Q
+ */
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface GrantPermissionRequestMapper extends HttpPermissionMapper<GrantPermissionRequest> {
+
+    @Override
+    @Mapping(source = "role",      target = "role.name")
+    @Mapping(source = "operation", target = "operation.name")
+    GrantedPermission toModel(GrantPermissionRequest request);
+
+    @Override
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "operation", ignore = true)
+    GrantPermissionRequest fromModel(GrantedPermission model);
+
+}
---
+++ infrastructure/adapter-rest/security/src/main/java/com/quathar/codebay/infra/security/model/mapper/HttpPermissionMapper.java
@@ -0,0 +1,18 @@
+package com.quathar.codebay.infra.security.model.mapper;
+
+import com.quathar.codebay.application.commonport.ModelMapperPort;
+import com.quathar.codebay.domain.model.security.GrantedPermission;
+
+/**
+ * <h1>HTTP (HyperText Transfer Protocol) Permission Mapper</h1>
+ *
+ * @param <R> The request or response to generate the mapper.
+ *
+ * @see GrantedPermission
+ * @see ModelMapperPort
+ * @since 2024-03-21
+ * @version 1.0
+ * @author Q
+ */
+public interface HttpPermissionMapper<R> extends ModelMapperPort<GrantedPermission, R> {
+}
