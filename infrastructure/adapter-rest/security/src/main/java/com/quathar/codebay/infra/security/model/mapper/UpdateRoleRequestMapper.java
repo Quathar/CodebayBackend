c11252b70db373994609d58bae182ad542709300
Q <99633210+Quathar@users.noreply.github.com>
2024-03-25 22:24:24 +0100

    Create Role & Operations request mappers

---
+++ infrastructure/adapter-rest/security/src/main/java/com/quathar/codebay/infra/security/model/mapper/UpdateRoleRequestMapper.java
@@ -0,0 +1,19 @@
+package com.quathar.codebay.infra.security.model.mapper;
+
+import com.quathar.codebay.infra.security.model.request.UpdateRoleRequest;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+/**
+ * <h1>Update Role Request Mapper</h1>
+ *
+ * @see UpdateRoleRequest
+ * @see HttpRoleMapper
+ * @since 2024-03-21
+ * @version 1.0
+ * @author Q
+ */
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UpdateRoleRequestMapper extends HttpRoleMapper<UpdateRoleRequest> {
+}
