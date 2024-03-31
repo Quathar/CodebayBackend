bc368e1982c61b4ca766ae32765d54f6ee211058
Q <99633210+Quathar@users.noreply.github.com>
2024-03-31 20:47:20 +0200

    Move Management API

---
+++ infrastructure/adapter-rest/management/src/main/java/com/quathar/codebay/infra/rest/api/ManagementAPI.java
@@ -0,0 +1,21 @@
+package com.quathar.codebay.infra.rest.api;
+
+import static com.quathar.codebay.infra.rest.common.api.BaseAPI.BASE_URL;
+
+/**
+ * <h1>Management API (Application Programming Interface)</h1>
+ *
+ * This interface defines endpoints and functionalities specific to administrative tasks within the application.
+ *
+ * @since 2023-12-19
+ * @version 1.0
+ * @author Q
+ */
+public interface ManagementAPI {
+
+    /**
+     * Base URL for the management API endpoints.
+     */
+    String MANAGE_URL = BASE_URL + "/management";
+
+}
