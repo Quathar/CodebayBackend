140f89fa1467611488865aaf8705683c6acbc883
Q <99633210+Quathar@users.noreply.github.com>
2024-03-31 20:48:37 +0200

    Create UserManagementController

--- infrastructure/adapter-rest/management/src/main/java/com/quathar/codebay/infra/rest/api/UserManagementAPI.java
+++ infrastructure/adapter-rest/management/src/main/java/com/quathar/codebay/infra/rest/api/UserManagementAPI.java
@@ -1,19 +1,26 @@
 package com.quathar.codebay.infra.rest.api;
 
-import com.quathar.codebay.domain.model.User;
-import com.quathar.codebay.infra.rest.api.crud.CreateModelEndpoint;
-import com.quathar.codebay.infra.rest.api.crud.DeleteModelEndpoint;
-import com.quathar.codebay.infra.rest.api.crud.RetrieveAllModelsEndpoint;
-import com.quathar.codebay.infra.rest.api.crud.RetrieveModelEndpoint;
-import com.quathar.codebay.infra.rest.api.crud.UpdateModelEndpoint;
-import com.quathar.codebay.infra.rest.model.request.CreateUserRequest;
-import com.quathar.codebay.infra.rest.model.request.UpdateUserRequest;
+import com.quathar.codebay.infra.rest.model.request.CreateManagementUserRequest;
+import com.quathar.codebay.infra.rest.common.model.request.PageContentRequest;
+import com.quathar.codebay.infra.rest.model.request.UpdateManagementUserRequest;
+import com.quathar.codebay.infra.rest.common.model.response.PageContentResponse;
+import com.quathar.codebay.infra.rest.model.response.FullUserResponse;
+import com.quathar.codebay.infra.rest.model.response.ManagementUserResponse;
+import com.quathar.codebay.infra.rest.security.annotation.AuthenticatedAccessByDefault;
 
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
 
 import static com.quathar.codebay.infra.rest.api.ManagementAPI.MANAGE_URL;
 
@@ -21,28 +28,93 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * <h1>User Management API</h1>
- * <br>
- * <p>
- *     This interface represents the User Management API, extending the base UserAPI.
- *     It provides endpoints and constants related to user management functionality.
- * </p>
  *
- * @see BaseAPI
+ * This interface represents the User Management API, extending the base UserAPI.
+ * It provides endpoints and constants related to user management functionality.
+ *
+ * @see ManagementAPI
  * @since 2023-12-19
  * @version 1.0
  * @author Q
  */
-@RequestMapping(UserManagementAPI.MANAGEMENT_ROOT)
-public interface UserManagementAPI
-       extends RetrieveAllModelsEndpoint<User>,
-               RetrieveModelEndpoint<User>,
-               CreateModelEndpoint<CreateUserRequest, User>,
-               UpdateModelEndpoint<UpdateUserRequest, User>,
-               DeleteModelEndpoint {
+@RequestMapping(UserManagementAPI.ROOT)
+//@RequiresAdminRole
+@AuthenticatedAccessByDefault
+public interface UserManagementAPI {
 
+    // <<-CONSTANT->>
     /**
      * The root path for user management API.
      */
-    String MANAGEMENT_ROOT = MANAGE_URL + "/users";
+    String ROOT = MANAGE_URL + "/users";
+
+    // <<-METHODS->>
+    /**
+     * Retrieves all content based on the provided PageContentRequest parameters.
+     *
+     * @param pageContentRequest The ContentRequest containing parameters for content retrieval.
+     * @return A ResponseEntity containing the retrieved content or an error response.
+     */
+    @GetMapping(path = "", produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    PageContentResponse<ManagementUserResponse> getAll(@Valid PageContentRequest pageContentRequest);
+
+    /**
+     * Retrieves a user by their unique ID.
+     *
+     * @param id The unique identifier of the model.
+     * @return ResponseEntity containing the model information if found.
+     */
+    @GetMapping(path = "/id/{id}", produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    FullUserResponse getById(@PathVariable UUID id);
+
+    /**
+     * Retrieves a user by their username.
+     *
+     * @param username The username of the user.
+     * @return UserManagementResponse containing the user information if found.
+     */
+    @GetMapping(path = "/username/{username}", produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    ManagementUserResponse getByUsername(@PathVariable String username);
+
+    /**
+     * Creates a new user.
+     *
+     * @param createRequest   The request object containing model information.
+     * @return ResponseEntity containing the newly created model information with appropriate status.
+     */
+    @PostMapping(value = "/sign-up",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    ManagementUserResponse signup(@RequestBody @Valid CreateManagementUserRequest createRequest);
+
+    /**
+     * Updates an existing user by their unique identifier.
+     *
+     * @param id            The unique identifier of the model to be updated.
+     * @param updateRequest The request object containing updated model information.
+     * @return
+     */
+    @PutMapping(value = "/{id}",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    ManagementUserResponse update(
+            @PathVariable UUID id,
+            @RequestBody @Valid
+            UpdateManagementUserRequest updateRequest
+    );
+
+    /**
+     * Deletes a model by their unique ID.
+     *
+     * @param id The unique identifier of the model to be deleted.
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable UUID id);
 
 }
---
+++ infrastructure/adapter-rest/management/src/main/java/com/quathar/codebay/infra/rest/controller/UserManagementController.java
@@ -0,0 +1,129 @@
+package com.quathar.codebay.infra.rest.controller;
+
+import com.quathar.codebay.application.inputport.UserServicePort;
+import com.quathar.codebay.domain.model.User;
+import com.quathar.codebay.infra.rest.api.UserManagementAPI;
+import com.quathar.codebay.infra.rest.common.factory.HttpCommonFactory;
+import com.quathar.codebay.infra.rest.factory.HttpManagementFactory;
+import com.quathar.codebay.infra.rest.model.request.CreateManagementUserRequest;
+import com.quathar.codebay.infra.rest.common.model.request.PageContentRequest;
+import com.quathar.codebay.infra.rest.model.request.UpdateManagementUserRequest;
+import com.quathar.codebay.infra.rest.common.model.response.PageContentResponse;
+import com.quathar.codebay.infra.rest.model.response.FullUserResponse;
+import com.quathar.codebay.infra.rest.model.response.ManagementUserResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+ /**
+ * <h1>User Management Controller</h1>
+ *
+ * @since 2023-12-10
+ * @version 1.0
+ * @author Q
+ */
+@RestController
+public class UserManagementController implements UserManagementAPI {
+
+     private static final Logger log = LoggerFactory.getLogger(UserManagementController.class);
+
+    // <<-FIELD->>
+     /**
+      * The port for user service.
+      */
+    private final UserServicePort userServicePort;
+
+    // <<-CONSTRUCTOR->>
+     /**
+      * Constructs a {@code UserManagementController} with the specified {@code UserServicePort}.
+      *
+      * @param userServicePort The port for user service.
+      */
+    @Autowired
+    public UserManagementController(UserServicePort userServicePort) {
+        this.userServicePort = userServicePort;
+    }
+
+    // <<-METHODS->>
+    @Override
+    public PageContentResponse<ManagementUserResponse> getAll(PageContentRequest pageContentRequest) {
+        Integer pageIndex = pageContentRequest.page();
+        Integer pageSize  = pageContentRequest.size();
+        java.util.List<ManagementUserResponse> response = this.userServicePort
+                .getAll(pageIndex, pageSize)
+                .stream()
+                .map(HttpManagementFactory.getManagementUserResponse()::fromModel)
+                .toList();
+
+        log.debug("Retrieving all management users with pageIndex {} and pageSize {}.", pageIndex, pageSize);
+
+        return HttpCommonFactory.getPageContentResponse(
+                pageIndex,
+                response.size(),
+                response
+        );
+    }
+
+    @Override
+    public FullUserResponse getById(UUID id) {
+        User user = this.userServicePort.getById(id);
+
+        log.debug("Retrieving management user with ID: {}", id);
+
+        return HttpManagementFactory
+                .getFullUserResponse()
+                .fromModel(user);
+    }
+
+     @Override
+     public ManagementUserResponse getByUsername(String username) {
+         User user = this.userServicePort.getByUsername(username);
+
+         log.debug("Retrieving management user with username: {}", username);
+
+         return HttpManagementFactory
+                 .getManagementUserResponse()
+                 .fromModel(user);
+     }
+
+     @Override
+     public ManagementUserResponse signup(CreateManagementUserRequest createRequest) {
+         User userToRegister = HttpManagementFactory
+                 .setCreateManagementUserRequest()
+                 .toModel(createRequest);
+         User createdUser = this.userServicePort.create(userToRegister);
+
+         log.debug("Creating management user: {}", createRequest);
+
+         return HttpManagementFactory
+                 .getManagementUserResponse()
+                 .fromModel(createdUser);
+     }
+
+     @Override
+     public ManagementUserResponse update(UUID id, UpdateManagementUserRequest updateRequest) {
+         User userToUpdate = HttpManagementFactory
+                 .setUpdateManagementUserRequest()
+                 .toModel(updateRequest);
+         userToUpdate.setId(id);
+         User updatedUser = this.userServicePort.update(userToUpdate);
+
+         log.debug("Updating management user with ID {}: {}", id, updateRequest);
+
+         return HttpManagementFactory
+                 .getManagementUserResponse()
+                 .fromModel(updatedUser);
+     }
+
+    @Override
+    public void delete(UUID id) {
+        log.debug("Deleting management user with ID: {}", id);
+        this.userServicePort.deleteById(id);
+    }
+
+}
