8acd218f727438c1625911d73217762678ce6ebc
Q <99633210+Quathar@users.noreply.github.com>
2024-04-04 20:34:15 +0200

    Rename & Update AdministratorServicePort

---
+++ application/src/main/java/com/quathar/codebay/application/inputport/AdministratorServicePort.java
@@ -0,0 +1,18 @@
+package com.quathar.codebay.application.inputport;
+
+import com.quathar.codebay.domain.model.Administrator;
+import com.quathar.codebay.domain.usecase.user.DeleteByUsernameUseCase;
+import com.quathar.codebay.domain.usecase.user.RetrieveByUsernameUseCase;
+
+/**
+ * <h1>Administrator Service Port</h1>
+ *
+ * @since 2023-12-10
+ * @version 1.0
+ * @author Q
+ */
+public interface AdministratorServicePort
+       extends   CrudServicePort<Administrator, java.util.UUID>,
+                 RetrieveByUsernameUseCase<Administrator>,
+                 DeleteByUsernameUseCase<Administrator> {
+}
