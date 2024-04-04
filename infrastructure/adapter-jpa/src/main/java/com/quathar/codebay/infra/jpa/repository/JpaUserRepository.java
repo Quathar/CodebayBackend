35aa2676357633e837b937d1752567b7d2f374b4
Q <99633210+Quathar@users.noreply.github.com>
2024-04-04 20:28:46 +0200

    Update JpaUserRepository

--- infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/JpaUserRepository.java
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/JpaUserRepository.java
@@ -3,10 +3,10 @@ package com.quathar.codebay.infra.jpa.repository;
 import com.quathar.codebay.infra.jpa.entity.UserEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 /**
  * <h1>JPA User Repository</h1>
@@ -16,7 +16,7 @@ import java.util.UUID;
  * @author Q
  */
 @Repository
-public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
+public interface JpaUserRepository extends JpaRepository<UserEntity, java.util.UUID> {
 
     /**
      * Finds a user by their username.
@@ -33,5 +33,22 @@ public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
      * @return An Optional containing the UserEntity associated with the provided email, if found.
      */
     Optional<UserEntity> findByEmail(String email);
+
+    /**
+     * Finds a user by their username or email.
+     * This method is primarily used for authentication purposes.
+     *
+     * @param authenticationKey The username or email to search for.
+     * @return An Optional containing the UserEntity associated with the provided username or email, if found.
+     */
+    @Query("SELECT u FROM UserEntity u WHERE u.username = :authenticationKey OR u.email = :authenticationKey")
+    Optional<UserEntity> findByAuthenticationKey(String authenticationKey);
+
+    /**
+     * Deletes a user by their username.
+     *
+     * @param username The username of the user to delete.
+     */
+    void deleteByUsername(String username);
 
 }
