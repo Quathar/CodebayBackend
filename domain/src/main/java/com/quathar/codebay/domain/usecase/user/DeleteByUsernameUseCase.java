def12f0ba16f239e53daeec7c57990bb4b9287b7
Q <99633210+Quathar@users.noreply.github.com>
2024-04-06 18:52:59 +0200

    Remove unnecessary generic in DeleteByUsernameUseCase

--- domain/src/main/java/com/quathar/codebay/domain/usecase/user/DeleteByUsernameUseCase.java
+++ domain/src/main/java/com/quathar/codebay/domain/usecase/user/DeleteByUsernameUseCase.java
@@ -1,22 +1,18 @@
 package com.quathar.codebay.domain.usecase.user;
 
-import com.quathar.codebay.domain.model.User;
-
 /**
- * <h1>Delete by username Use Case</h1>
- *
- * @param <M> The type of the user model
+ * <h1>Delete by Username Use Case</h1>
  *
  * @since 2024-04-02
  * @version 1.0
  * @author Q
  */
-public interface DeleteByUsernameUseCase<M extends User> {
+public interface DeleteByUsernameUseCase {
 
     /**
      * Deletes a user by the username.
      *
-     * @param username the username of the user to delete
+     * @param username The username of the user to delete
      */
     void deleteByUsername(String username);
 
