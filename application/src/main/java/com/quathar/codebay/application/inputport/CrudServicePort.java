e5d5c4894740837f700c952c8ab423166c35a301
Q <99633210+Quathar@users.noreply.github.com>
2024-04-07 20:40:41 +0200

    [UPDATE] CrudServicePort references

--- application/src/main/java/com/quathar/codebay/application/inputport/CrudServicePort.java
+++ application/src/main/java/com/quathar/codebay/application/inputport/CrudServicePort.java
@@ -1,16 +1,15 @@
 package com.quathar.codebay.application.inputport;
 
-import com.quathar.codebay.application.usecase.crud.CreateModelUseCase;
-import com.quathar.codebay.application.usecase.crud.DeleteModelUseCase;
-import com.quathar.codebay.application.usecase.crud.ReadModelUseCase;
-import com.quathar.codebay.application.usecase.crud.UpdateModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.CreateModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.DeleteModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.ReadAllModelsUseCase;
+import com.quathar.codebay.domain.usecase.crud.ReadModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.UpdateModelUseCase;
 
 /**
  * <h1>CRUD Service Port</h1>
- * <br>
- * <p>
- *     Contract for the basic CRUD <b>use cases</b>:
- * </p>
+ *
+ * Contract for the basic CRUD <b>use cases</b>:
  * <ul>
  *     <li>Create</li>
  *     <li>Read</li>
@@ -25,5 +24,10 @@ import com.quathar.codebay.application.usecase.crud.UpdateModelUseCase;
  * @version 1.0
  * @author Q
  */
-public interface CrudServicePort<M, ID> extends CreateModelUseCase<M>, ReadModelUseCase<M, ID>, UpdateModelUseCase<M>, DeleteModelUseCase<ID> {
+public interface CrudServicePort<M, ID>
+       extends   CreateModelUseCase<M>,
+                 ReadAllModelsUseCase<M>,
+                 ReadModelUseCase<M, ID>,
+                 UpdateModelUseCase<M>,
+                 DeleteModelUseCase<ID> {
 }
