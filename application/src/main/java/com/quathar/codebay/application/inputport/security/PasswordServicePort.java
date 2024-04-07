60d1cf6f2bebf392e1ea30352ad4fedf26c1b7d8
Q <99633210+Quathar@users.noreply.github.com>
2024-04-07 22:21:02 +0200

    [RENAME] PasswordServicePort

--- application/src/main/java/com/quathar/codebay/application/inputport/security/PasswordServicePort.java
+++ application/src/main/java/com/quathar/codebay/application/inputport/security/PasswordServicePort.java
@@ -4,11 +4,11 @@ import com.quathar.codebay.domain.usecase.security.EncodePasswordUseCase;
 import com.quathar.codebay.domain.usecase.security.PasswordMatchesUseCase;
 
 /**
- * <h1>Password Encoder Port</h1>
+ * <h1>Password Service Port</h1>
  *
  * @since 2024-04-04
  * @version 1.0
  * @author Q
  */
-public interface PasswordEncoderPort extends EncodePasswordUseCase, PasswordMatchesUseCase {
+public interface PasswordServicePort extends EncodePasswordUseCase, PasswordMatchesUseCase {
 }
