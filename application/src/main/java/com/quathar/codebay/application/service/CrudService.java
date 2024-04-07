4dc332868671d91a8f4dc923afbf708c75110eea
Q <99633210+Quathar@users.noreply.github.com>
2024-04-07 21:06:10 +0200

    [UPDATE] CRUD Service

--- application/src/main/java/com/quathar/codebay/application/service/CrudService.java
+++ application/src/main/java/com/quathar/codebay/application/service/CrudService.java
@@ -1,8 +1,14 @@
 package com.quathar.codebay.application.service;
 
 import com.quathar.codebay.application.inputport.CrudServicePort;
-import com.quathar.codebay.application.outputport.CrudRepositoryPort;
-import com.quathar.codebay.domain.exception.ResourceNotFoundException;
+import com.quathar.codebay.domain.usecase.crud.CreateModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.DeleteModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.ReadAllModelsUseCase;
+import com.quathar.codebay.domain.usecase.crud.ReadModelUseCase;
+import com.quathar.codebay.domain.usecase.crud.UpdateModelUseCase;
+
+import lombok.NonNull;
+import lombok.experimental.SuperBuilder;
 
 /**
  * <h1>CRUD Service</h1>
@@ -16,48 +22,40 @@ import com.quathar.codebay.domain.exception.ResourceNotFoundException;
  * @version 1.0
  * @author Q
  */
-public class CrudService<M, ID> implements CrudServicePort<M, ID> {
+@SuperBuilder(builderMethodName = "setup", buildMethodName = "seal")
+public abstract class CrudService<M, ID> implements CrudServicePort<M, ID> {
 
-    // <<-FIELD->>
-    /**
-     * The port providing access to CRUD operations for the specified domain model.
-     */
-    private final CrudRepositoryPort<M, ID> crudRepositoryPort;
-
-    // <<-CONSTRUCTOR->>
-    /**
-     * Constructs a CrudService with the provided CrudRepositoryPort.
-     *
-     * @param crudRepositoryPort The port providing access to CRUD operations for the specified domain model.
-     */
-    public CrudService(CrudRepositoryPort<M, ID> crudRepositoryPort) {
-        this.crudRepositoryPort = crudRepositoryPort;
-    }
+    // <<-FIELDS->>
+    @NonNull private final ReadAllModelsUseCase<M> readAllUseCase;
+    @NonNull private final ReadModelUseCase<M, ID> readUseCase;
+    @NonNull private final CreateModelUseCase<M>   createUseCase;
+    @NonNull private final UpdateModelUseCase<M>   updateUseCase;
+    @NonNull private final DeleteModelUseCase<ID>  deleteUseCase;
 
     // <<-METHODS->>
     @Override
     public java.util.List<M> getAll(int pageIndex, int pageSize) {
-        return this.crudRepositoryPort.findAll(pageIndex, pageSize);
+        return this.readAllUseCase.getAll(pageIndex, pageSize);
     }
 
     @Override
-    public M getById(ID id) throws ResourceNotFoundException {
-        return this.crudRepositoryPort.findById(id).orElseThrow(ResourceNotFoundException::new);
+    public M getById(ID id) {
+        return this.readUseCase.getById(id);
     }
 
     @Override
     public M create(M model) {
-        return this.crudRepositoryPort.save(model);
+        return this.createUseCase.create(model);
     }
 
     @Override
     public M update(M model) {
-        return this.crudRepositoryPort.update(model);
+        return this.updateUseCase.update(model);
     }
 
     @Override
     public void deleteById(ID id) {
-        this.crudRepositoryPort.deleteById(id);
+        this.deleteUseCase.deleteById(id);
     }
 
 }
