bcafe8e529b1f06cff167a40ac74bb50a7820dd0
Q <99633210+Quathar@users.noreply.github.com>
2024-04-12 17:19:16 +0200

    [update] infrastructure\build.gradle.kts

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,2 +1,44 @@
-dependencies {
+subprojects {
+    dependencies {
+        // [ Modules ]
+        implementation( project(":${ Module.DOMAIN }") )
+        implementation( project(":${ Module.APP    }") )
+    }
+}
+
+// <<-PROJECTS CONFIGURATION->>
+listOf(Module.CONFIGURATION, Module.SECURITY_ADAPTER, Module.JPA_ADAPTER)
+        .forEach { projectName ->
+            project(":$projectName") {
+                tasks.getByName("bootJar") {
+                    enabled = false
+                }
+
+                tasks.getByName("jar") {
+                    enabled = true
+                }
+            }
+        }
+
+project(":${ Module.JPA_ADAPTER }") {
+    dependencies {
+        implementation     ( Lib.SPRING_BOOT_DATA_JPA )
+        implementation     ( Lib.MAPSTRUCT            )
+        annotationProcessor( Lib.MAPSTRUCT_PROCESSOR  )
+    }
+}
+
+project(":${ Module.SECURITY_ADAPTER }") {
+    dependencies {
+        implementation( Lib.SPRING_BOOT_SECURITY )
+        implementation( Lib.AUTH0_JWT            )
+    }
+}
+
+project(":${ Module.CONFIGURATION }") {
+    dependencies {
+        implementation( project(":${ Module.SECURITY_ADAPTER }") )
+        implementation( project(":${ Module.JPA_ADAPTER      }") )
+        implementation( Lib.SPRING_CONTEXT                       )
+    }
 }
