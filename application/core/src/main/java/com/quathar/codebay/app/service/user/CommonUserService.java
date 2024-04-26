b345a6dd20264a0c1b45cafed2f6cb08f38bf901
Q <99633210+Quathar@users.noreply.github.com>
2024-04-26 17:47:06 +0200

    feat: Create Common User Service

---
+++ application/core/src/main/java/com/quathar/codebay/app/service/user/CommonUserService.java
@@ -0,0 +1,45 @@
+package com.quathar.codebay.app.service.user;
+
+import com.quathar.codebay.app.service.CrudService;
+import com.quathar.codebay.domain.model.User;
+import com.quathar.codebay.domain.port.in.user.DeleteByUsernameUseCasePort;
+import com.quathar.codebay.domain.port.in.user.ReadByUsernameUseCasePort;
+
+import lombok.NonNull;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <h1>Common User Service</h1>
+ *
+ * @param <M> The user type
+ *
+ * @see ReadByUsernameUseCasePort
+ * @see DeleteByUsernameUseCasePort
+ * @since 2024-04-14
+ * @version 1.0
+ * @author Q
+ */
+@SuperBuilder(builderMethodName = "setup", buildMethodName = "seal")
+public sealed class CommonUserService<M extends User>
+       extends      CrudService<M, java.util.UUID>
+       implements   ReadByUsernameUseCasePort<M>,
+                    DeleteByUsernameUseCasePort
+       permits      UserService,
+                    AdministratorService {
+
+    // <<-FIELDS->>
+    @NonNull protected final ReadByUsernameUseCasePort<M> readByUsernameUseCase;
+    @NonNull protected final DeleteByUsernameUseCasePort  deleteByUsernameUseCase;
+
+    // <<-METHODS->>
+    @Override
+    public M getByUsername(String username) {
+        return this.readByUsernameUseCase.getByUsername(username);
+    }
+
+    @Override
+    public void deleteByUsername(String username) {
+        this.deleteByUsernameUseCase.deleteByUsername(username);
+    }
+
+}
