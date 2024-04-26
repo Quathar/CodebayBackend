2b819121e2ffaf25634a38e2e44216d37d37bc8e
Q <99633210+Quathar@users.noreply.github.com>
2024-04-26 18:28:43 +0200

    feat: Create ManagementAPI

---
+++ application/rest/src/main/java/com/quathar/codebay/app/rest/management/api/ManagementAPI.java
@@ -0,0 +1,21 @@
+package com.quathar.codebay.app.rest.management.api;
+
+import static com.quathar.codebay.app.rest.common.api.BaseAPI.BASE_URL;
+
+/**
+ * <h1>Management API (Application Programming Interface)</h1>
+ *
+ * This interface defines endpoints and functionalities specific to administrative tasks within the application.
+ *
+ * @since 2023-12-19
+ * @version 1.0
+ * @author Q
+ */
+public interface ManagementAPI {
+
+    /**
+     * Base URL for the management API endpoints.
+     */
+    String MANAGEMENT_URL = BASE_URL + "/management";
+
+}
