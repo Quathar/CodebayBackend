efbf36889fa12062795ea25df2d1de7e83c839cf
Q <99633210+Quathar@users.noreply.github.com>
2024-04-26 17:47:33 +0200

    feat: Create users final services

---
+++ application/core/src/main/java/com/quathar/codebay/app/service/user/UserService.java
@@ -0,0 +1,18 @@
+package com.quathar.codebay.app.service.user;
+
+import com.quathar.codebay.domain.model.User;
+
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <h1>User Service</h1>
+ *
+ * @see CommonUserService
+ * @see User
+ * @since 2023-12-10
+ * @version 1.0
+ * @author Q
+ */
+@SuperBuilder(builderMethodName = "setup", buildMethodName = "seal")
+public final class UserService extends CommonUserService<User> {
+}
