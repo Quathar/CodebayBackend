227a51dec014a0a0b4e1e932943f8e3996896fc4
Q <99633210+Quathar@users.noreply.github.com>
2024-05-02 21:56:35 +0200

    feat: Create User & Admin entities

---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/entity/AdministratorEntity.java
@@ -0,0 +1,35 @@
+package com.quathar.codebay.infra.jpa.entity;
+
+import com.quathar.codebay.infra.jpa.embeddable.PersonalInfoEmbeddable;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * <h1>Administrator Entity</h1>
+ *
+ * @see com.quathar.codebay.domain.model.Administrator
+ * @see UserEntity
+ * @since 2023-12-19
+ * @version 1.0
+ * @author Q
+ */
+@Entity
+@Table(name = "Administrator")
+@PrimaryKeyJoinColumn(name = "user_id", referencedColumnName = "id")
+@NoArgsConstructor
+@Getter
+@Setter
+public final class AdministratorEntity extends UserEntity {
+
+    // <<-FIELD->>
+    @Embedded
+    private PersonalInfoEmbeddable personalInfo;
+
+}
