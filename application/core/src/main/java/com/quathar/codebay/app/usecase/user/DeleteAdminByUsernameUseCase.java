869750f705880da22180a74a35ebbb0e6d0dca70
Q <99633210+Quathar@users.noreply.github.com>
2024-05-02 23:45:57 +0200

    refactor: Update DeleteAdminByUsernameUseCase

--- application/core/src/main/java/com/quathar/codebay/app/usecase/user/DeleteAdminByUsernameUseCase.java
+++ application/core/src/main/java/com/quathar/codebay/app/usecase/user/DeleteAdminByUsernameUseCase.java
@@ -24,7 +24,7 @@ public final class DeleteAdminByUsernameUseCase implements DeleteByUsernameUseCa
      */
     private static final String DISABLED_STATUS = "DISABLED";
 
-    // <<-CONSTANT->>
+    // <<-FIELD->>
     /**
      * The administrator repository port.
      */
