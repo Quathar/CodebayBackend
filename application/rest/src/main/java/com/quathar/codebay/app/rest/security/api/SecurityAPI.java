dbd379121d32ee8d0ff11717314433b21f993fa3
Q <99633210+Quathar@users.noreply.github.com>
2024-05-02 03:36:26 +0200

    feat: Create SecurityAPI

---
+++ application/rest/src/main/java/com/quathar/codebay/app/rest/security/api/SecurityAPI.java
@@ -0,0 +1,19 @@
+package com.quathar.codebay.app.rest.security.api;
+
+import static com.quathar.codebay.app.rest.common.api.BaseAPI.BASE_URL;
+
+/**
+ * <h1>Security API</h1>
+ *
+ * @since 2024-03-21
+ * @version 1.0
+ * @author Q
+ */
+public interface SecurityAPI {
+
+    /**
+     * Base URL for security API endpoints.
+     */
+    String SECURITY_URL = BASE_URL + "/security";
+
+}
