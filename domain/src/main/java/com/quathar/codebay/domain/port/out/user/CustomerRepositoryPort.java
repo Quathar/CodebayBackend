4c16f6eb0475f62f1eb61c8f82aa4e276b9f7f03
Q <99633210+Quathar@users.noreply.github.com>
2024-05-15 15:12:03 +0200

    feat: Create CustomerRepositoryPort

---
+++ domain/src/main/java/com/quathar/codebay/domain/port/out/user/CustomerRepositoryPort.java
@@ -0,0 +1,18 @@
+package com.quathar.codebay.domain.port.out.user;
+
+import com.quathar.codebay.domain.model.Customer;
+
+/**
+ * <h1>Customer Repository Port</h1>
+ *
+ * Represents a contract for managing customer domain models.
+ *
+ * @see GenericUserRepositoryPort
+ * @see Customer
+ * @since 2024-05-07
+ * @version 1.0
+ * @author Q
+ */
+public interface CustomerRepositoryPort
+       extends   GenericUserRepositoryPort<Customer, java.util.UUID> {
+}
