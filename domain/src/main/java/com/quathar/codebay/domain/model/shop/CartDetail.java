28ee242645ee3fbc194780e129318335bbcd0717
Q <99633210+Quathar@users.noreply.github.com>
2024-05-15 14:51:17 +0200

    update: Add stream() method in CartDetail

--- domain/src/main/java/com/quathar/codebay/domain/model/shop/CartDetail.java
+++ domain/src/main/java/com/quathar/codebay/domain/model/shop/CartDetail.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.stream.Stream;
+
 /**
  * <h1>Shopping Cart Detail Model</h1>
  *
@@ -19,5 +21,15 @@ public final class CartDetail {
     // <<-FIELDS->>
     private Product product;
     private Integer units;
+
+    // <<-METHOD->>
+    /**
+     * Returns a sequential {@code Stream} containing only this cart detail.
+     *
+     * @return A sequential {@code Stream} containing only this cart detail
+     */
+    public Stream<CartDetail> stream() {
+        return Stream.of(this);
+    }
 
 }
