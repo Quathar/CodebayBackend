a1d858d53f764bc89ae6538808c0b3fb6aaf13a0
Q <99633210+Quathar@users.noreply.github.com>
2024-05-25 17:37:47 +0200

    feat: Create CustomerMapper

---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/mapper/CustomerMapper.java
@@ -0,0 +1,31 @@
+package com.quathar.codebay.infra.jpa.mapper;
+
+import com.quathar.codebay.domain.model.Customer;
+import com.quathar.codebay.domain.port.out.mapper.MapperServicePort;
+import com.quathar.codebay.infra.jpa.embeddable.mapper.AuditMapper;
+import com.quathar.codebay.infra.jpa.embeddable.mapper.PersonalInfoMapper;
+import com.quathar.codebay.infra.jpa.entity.CustomerEntity;
+import com.quathar.codebay.infra.jpa.mapper.security.RoleMapper;
+
+import org.mapstruct.Mapper;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+/**
+ * <h1>Customer Mapper</h1>
+ *
+ * @see MapperServicePort
+ * @see Customer
+ * @see CustomerEntity
+ * @since 2024-05-07
+ * @version 1.0
+ * @author Q
+ */
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = IGNORE, uses = {
+        RoleMapper.class,
+        AuditMapper.class,
+        PersonalInfoMapper.class
+})
+public interface CustomerMapper extends MapperServicePort<Customer, CustomerEntity> {
+}
