ec8153f2baa7c76d4eeec775dc0dc9351210ff96
Q <99633210+Quathar@users.noreply.github.com>
2024-05-25 17:19:11 +0200

    feat: Create shop entities & main embeddables

---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/entity/shop/CartDetailEmbeddable.java
@@ -0,0 +1,32 @@
+package com.quathar.codebay.infra.jpa.entity.shop;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * <h1>Cart Detail Embeddable</h1>
+ *
+ * @see com.quathar.codebay.domain.model.shop.CartDetail
+ * @since 2023-05-13
+ * @version 1.0
+ * @author Q
+ */
+@Embeddable
+@NoArgsConstructor
+@Getter
+@Setter
+public final class CartDetailEmbeddable {
+
+    // <<-FIELDS->>
+    @OneToOne(optional = false)
+    private ProductEntity product;
+
+    @Column(nullable = false)
+    private Integer units;
+
+}
---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/entity/shop/OrderEntity.java
@@ -0,0 +1,69 @@
+package com.quathar.codebay.infra.jpa.entity.shop;
+
+import com.quathar.codebay.infra.jpa.entity.CustomerEntity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * <h1>Order Entity</h1>
+ *
+ * @see com.quathar.codebay.domain.model.shop.Order
+ * @since 2023-03-27
+ * @version 1.0
+ * @author Q
+ */
+@Entity
+@Table(name = "Orders")
+@NoArgsConstructor
+@Getter
+@Setter
+public final class OrderEntity {
+
+    // <<-FIELDS->>
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customer;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(name = "delivery_date")
+    private LocalDateTime deliveryDate;
+
+    @ElementCollection
+    @CollectionTable(name = "order_details",
+            joinColumns = @JoinColumn(name = "order_id"),
+            foreignKey  = @ForeignKey(name = "FK_order-details_order_id"))
+    private List<OrderDetailEmbeddable> details;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private String status;
+
+}
