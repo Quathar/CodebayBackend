cc186aac82ee5760cfe63cd609121f0d844edaf1
Q <99633210+Quathar@users.noreply.github.com>
2024-05-26 01:15:43 +0200

    update: Add new use case & clean CartService

--- application/core/src/main/java/com/quathar/codebay/app/service/shop/ShoppingCartService.java
+++ application/core/src/main/java/com/quathar/codebay/app/service/shop/ShoppingCartService.java
@@ -2,7 +2,12 @@ package com.quathar.codebay.app.service.shop;
 
 import com.quathar.codebay.domain.model.shop.Order;
 import com.quathar.codebay.domain.model.shop.ShoppingCart;
-import com.quathar.codebay.domain.port.in.shop.cart.*;
+import com.quathar.codebay.domain.port.in.shop.cart.AddProductToCartUseCasePort;
+import com.quathar.codebay.domain.port.in.shop.cart.BuyCartProductsUseCasePort;
+import com.quathar.codebay.domain.port.in.shop.cart.MoveProductToWishListUseCasePort;
+import com.quathar.codebay.domain.port.in.shop.cart.ReadCartByUsernameUseCasePort;
+import com.quathar.codebay.domain.port.in.shop.cart.RemoveProductFromCartUseCasePort;
+import com.quathar.codebay.domain.port.in.shop.cart.UpdateProductInCartUseCasePort;
 
 import lombok.Builder;
 import lombok.NonNull;
@@ -12,6 +17,7 @@ import lombok.NonNull;
  *
  * @see ReadCartByUsernameUseCasePort
  * @see AddProductToCartUseCasePort
+ * @see UpdateProductInCartUseCasePort
  * @see RemoveProductFromCartUseCasePort
  * @see MoveProductToWishListUseCasePort
  * @see BuyCartProductsUseCasePort
@@ -23,13 +29,15 @@ import lombok.NonNull;
 public final class ShoppingCartService
        implements  ReadCartByUsernameUseCasePort,
                    AddProductToCartUseCasePort,
+                   UpdateProductInCartUseCasePort,
                    RemoveProductFromCartUseCasePort,
                    MoveProductToWishListUseCasePort,
                    BuyCartProductsUseCasePort {
 
     // <<-FIELDS->>
-    @NonNull private final ReadCartByUsernameUseCasePort    readCartByUsernameUseCasePort;
+    @NonNull private final ReadCartByUsernameUseCasePort    readCartByUsernameUseCase;
     @NonNull private final AddProductToCartUseCasePort      addProductToCartUseCase;
+    @NonNull private final UpdateProductInCartUseCasePort   updateProductInCartUseCase;
     @NonNull private final RemoveProductFromCartUseCasePort removeProductFromCartUseCase;
     @NonNull private final MoveProductToWishListUseCasePort moveProductToWishListUseCase;
     @NonNull private final BuyCartProductsUseCasePort       buyCartProductsUseCase;
@@ -37,12 +45,17 @@ public final class ShoppingCartService
     // <<-METHODS->>
     @Override
     public ShoppingCart getCartByUsername(String username) {
-        return this.readCartByUsernameUseCasePort.getCartByUsername(username);
+        return this.readCartByUsernameUseCase.getCartByUsername(username);
     }
 
     @Override
     public ShoppingCart addToCart(String username, String productId, int units) {
         return this.addProductToCartUseCase.addToCart(username, productId, units);
+    }
+
+    @Override
+    public ShoppingCart updateInCart(String username, String productCode, int units) {
+        return this.updateProductInCartUseCase.updateInCart(username, productCode, units);
     }
 
     @Override
