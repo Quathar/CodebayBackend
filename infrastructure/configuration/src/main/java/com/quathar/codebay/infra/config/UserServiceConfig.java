d9d1d1cc067fe13cc3ae9f13dc8fdf4891851411
Q <99633210+Quathar@users.noreply.github.com>
2024-07-24 21:17:15 +0200

    feat() Add new use cases in UserServiceConfig

--- infrastructure/configuration/src/main/java/com/quathar/codebay/infra/config/UserServiceConfig.java
+++ infrastructure/configuration/src/main/java/com/quathar/codebay/infra/config/UserServiceConfig.java
@@ -3,17 +3,21 @@ package com.quathar.codebay.infra.config;
 import com.quathar.codebay.app.service.user.AdministratorService;
 import com.quathar.codebay.app.service.user.CustomerService;
 import com.quathar.codebay.app.service.user.UserService;
-import com.quathar.codebay.app.usecase.CreateModelUseCase;
 import com.quathar.codebay.app.usecase.DeleteModelUseCase;
 import com.quathar.codebay.app.usecase.ReadAllModelsUseCase;
 import com.quathar.codebay.app.usecase.ReadModelUseCase;
 import com.quathar.codebay.app.usecase.UpdateModelUseCase;
 import com.quathar.codebay.app.usecase.user.CreateAdminUseCase;
+import com.quathar.codebay.app.usecase.user.CreateCustomerUseCase;
 import com.quathar.codebay.app.usecase.user.CreateUserUseCase;
 import com.quathar.codebay.app.usecase.user.DeleteAbstractUserByIdUseCase;
 import com.quathar.codebay.app.usecase.user.DeleteAbstractUserByUsernameUseCase;
 import com.quathar.codebay.app.usecase.user.ReadAbstractUserByUsernameUseCase;
+import com.quathar.codebay.app.usecase.user.UpdateAdminByUsernameUseCase;
+import com.quathar.codebay.app.usecase.user.UpdateCustomerByUsernameUseCase;
 import com.quathar.codebay.app.usecase.user.UpdateUserByUsernameUseCase;
+import com.quathar.codebay.domain.port.out.shop.ShoppingCartRepositoryPort;
+import com.quathar.codebay.domain.port.out.shop.WishListRepositoryPort;
 import com.quathar.codebay.domain.port.out.user.AdministratorRepositoryPort;
 import com.quathar.codebay.domain.port.out.user.CustomerRepositoryPort;
 import com.quathar.codebay.domain.port.out.user.UserRepositoryPort;
@@ -63,21 +67,26 @@ public class UserServiceConfig {
                 .readByUsernameUseCase( new ReadAbstractUserByUsernameUseCase<>(adminRepository) )
                 .createUseCase( new CreateAdminUseCase(adminRepository, roleRepository, passwordService) )
                 .updateUseCase( new UpdateModelUseCase<>(adminRepository) )
+                .updateByUsernameUseCase( new UpdateAdminByUsernameUseCase(adminRepository) )
                 .deleteUseCase( new DeleteAbstractUserByIdUseCase<>(adminRepository) )
                 .deleteByUsernameUseCase( new DeleteAbstractUserByUsernameUseCase<>(adminRepository) )
                 .seal();
     }
 
     @Bean
-    public CustomerService customerService(CustomerRepositoryPort customerRepository) {
-        // WARNING: THIS IS NOT COMPLETE
-        // TODO: Create missing use cases
+    public CustomerService customerService(
+            CustomerRepositoryPort     customerRepository,
+            ShoppingCartRepositoryPort cartRepository,
+            WishListRepositoryPort     wishListRepository,
+            RoleRepositoryPort         roleRepository
+    ) {
         return CustomerService.setup()
                 .readAllUseCase( new ReadAllModelsUseCase<>(customerRepository) )
                 .readUseCase( new ReadModelUseCase<>(customerRepository) )
                 .readByUsernameUseCase( new ReadAbstractUserByUsernameUseCase<>(customerRepository) )
-                .createUseCase( new CreateModelUseCase<>(customerRepository) )
+                .createUseCase( new CreateCustomerUseCase(customerRepository, cartRepository, wishListRepository, roleRepository) )
                 .updateUseCase( new UpdateModelUseCase<>(customerRepository) )
+                .updateByUsernameUseCase( new UpdateCustomerByUsernameUseCase(customerRepository) )
                 .deleteUseCase( new DeleteModelUseCase<>(customerRepository) )
                 .deleteByUsernameUseCase( new DeleteAbstractUserByUsernameUseCase<>(customerRepository) )
                 .seal();
