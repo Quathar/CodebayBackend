172d3befe1246d37b3ee1031ad5aeba37ab7a86e
Q <99633210+Quathar@users.noreply.github.com>
2024-07-24 20:45:25 +0200

    feat() Create insertCustomer() custom HQL query

--- infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/user/JpaCustomerRepository.java
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/user/JpaCustomerRepository.java
@@ -2,6 +2,11 @@ package com.quathar.codebay.infra.jpa.repository.user;
 
 import com.quathar.codebay.infra.jpa.entity.CustomerEntity;
 
+import jakarta.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,5 +19,38 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface JpaCustomerRepository
-       extends   CommonJpaUserRepository<CustomerEntity> {
+       extends   JpaAbstractUserRepository<CustomerEntity> {
+
+    /**
+     * Inserts a new customer record into the Customer table.
+     *
+     * @param customer The {@link CustomerEntity} containing all the necessary information to be inserted.
+     * @return The number of rows affected by the insert operation.
+     */
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(nativeQuery = true, value = """
+        INSERT INTO Customer(
+            user_id,
+            document_type, document,
+            name, surnames, gender, birthdate,
+            phone_code, phone_number,
+            country, road_type, address_name, address_number, portal,
+            floor, location, region, postal_code,
+            accumulated_expenditure, type, license,
+            comments
+        )
+        VALUES (
+            :#{#customer.id},
+            :#{#customer.personalInfo.document.type}, :#{#customer.personalInfo.document.content},
+            :#{#customer.personalInfo.name}, :#{#customer.personalInfo.surnames}, :#{#customer.personalInfo.gender}, :#{#customer.personalInfo.birthdate},
+            :#{#customer.personalInfo.phone.countryCode}, :#{#customer.personalInfo.phone.number},
+            :#{#customer.country}, :#{#customer.homeAddress.roadType}, :#{#customer.homeAddress.name}, :#{#customer.homeAddress.number}, :#{#customer.homeAddress.portal},
+            :#{#customer.homeAddress.floor}, :#{#customer.homeAddress.location}, :#{#customer.homeAddress.region}, :#{#customer.homeAddress.postalCode},
+            :#{#customer.accumulatedExpenditure}, :#{#customer.type}, :#{#customer.license},
+            :#{#customer.comments}
+        );
+    """)
+    int insertCustomer(@Param("customer") CustomerEntity customer);
+
 }
