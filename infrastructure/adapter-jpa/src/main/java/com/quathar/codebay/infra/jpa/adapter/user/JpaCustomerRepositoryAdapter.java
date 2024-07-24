ec2e9656c9381d6c11a72b195c2372ab281c9490
Q <99633210+Quathar@users.noreply.github.com>
2024-07-24 21:09:59 +0200

    update() Change the logic when saving a new Customer for 1st time

--- infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/adapter/user/JpaCustomerRepositoryAdapter.java
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/adapter/user/JpaCustomerRepositoryAdapter.java
@@ -1,16 +1,17 @@
 package com.quathar.codebay.infra.jpa.adapter.user;
 
+import com.quathar.codebay.domain.exception.ModelNotFoundException;
 import com.quathar.codebay.domain.model.Customer;
 import com.quathar.codebay.domain.port.out.user.CustomerRepositoryPort;
-import com.quathar.codebay.infra.jpa.adapter.JpaCrudRepositoryAdapter;
 import com.quathar.codebay.infra.jpa.entity.CustomerEntity;
 import com.quathar.codebay.infra.jpa.mapper.CustomerMapper;
 import com.quathar.codebay.infra.jpa.repository.user.JpaCustomerRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 /**
  * <h1>JPA Customer Repository Adapter</h1>
@@ -24,8 +25,10 @@ import java.util.Optional;
  */
 @Component
 public final class JpaCustomerRepositoryAdapter
-       extends     JpaCrudRepositoryAdapter<Customer, CustomerEntity, java.util.UUID>
+       extends     JpaAbstractUserRepositoryAdapter<Customer, CustomerEntity>
        implements  CustomerRepositoryPort {
+
+    private static final Logger log = LoggerFactory.getLogger(JpaCustomerRepositoryAdapter.class);
 
     // <<-FIELDS->>
     /**
@@ -52,22 +55,42 @@ public final class JpaCustomerRepositoryAdapter
 
     // <<-METHODS->>
     @Override
-    public Optional<Customer> findByUsername(String username) {
-        return this.jpaCustomerRepository
-                .findByUsername(username)
-                .map(this.customerMapper::toModel);
-    }
+    public Customer save(Customer customerToCreate) {
+        /*
+        You may wonder why I don't use the 'save()' default method directly.
 
-    @Override
-    public Optional<Customer> findByEmail(String email) {
-        return this.jpaCustomerRepository
-                .findByEmail(email)
-                .map(this.customerMapper::toModel);
-    }
+        I've tried.
 
-    @Override
-    public void deleteByUsername(String username) {
-        this.jpaCustomerRepository.deleteByUsername(username);
+        If you only set the ID of the preexistent User
+        then 'IntegrityConstraintViolationException' is thrown since we're
+        trying to insert in the 'Users' table and a lot of fields
+        cannot be null.
+
+        If we put all the fields of the preexistent User
+        then 'DataIntegrityViolationException' is thrown
+        as some of the fields are being duplicated.
+
+        This happens because JPA tries to insert in 'User' table in both cases,
+        and we DON'T WANT that.
+
+        We only want to insert in 'Customer' table, so the solution here is to create
+        a custom Query in the 'JPA Customer Repository'.
+
+        This solution was very PAINFUL.
+        */
+        CustomerEntity entityToCreate = this.customerMapper.fromModel(customerToCreate);
+        int affectedRows = this.jpaCustomerRepository.insertCustomer(entityToCreate);
+
+        log.debug("Affected rows: {}", affectedRows);
+
+        if (affectedRows == 0)
+            // TODO: We could use some custom exception here
+            throw new RuntimeException();
+
+        return this.jpaCustomerRepository
+                .findById(customerToCreate.getId())
+                .map(this.customerMapper::toModel)
+                .orElseThrow(ModelNotFoundException::new);
     }
 
 }
