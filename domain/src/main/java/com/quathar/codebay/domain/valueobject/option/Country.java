decbad69fb1f7a5ea8bf4567535991bc248f6395
Q <99633210+Quathar@users.noreply.github.com>
2024-07-27 18:48:22 +0200

    feat() Create Option value objects

---
+++ domain/src/main/java/com/quathar/codebay/domain/valueobject/option/Country.java
@@ -0,0 +1,15 @@
+package com.quathar.codebay.domain.valueobject.option;
+
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <h1>Country Value Object</h1>
+ *
+ * @see BaseOption
+ * @since 2023-05-13
+ * @version 1.0
+ * @author Q
+ */
+@SuperBuilder
+public final class Country extends BaseOption {
+}
---
+++ domain/src/main/java/com/quathar/codebay/domain/valueobject/option/RoadType.java
@@ -0,0 +1,15 @@
+package com.quathar.codebay.domain.valueobject.option;
+
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <h1>Road Type Value Object</h1>
+ *
+ * @see BaseOption
+ * @since 2023-05-13
+ * @version 1.0
+ * @author Q
+ */
+@SuperBuilder
+public final class RoadType extends BaseOption {
+}
