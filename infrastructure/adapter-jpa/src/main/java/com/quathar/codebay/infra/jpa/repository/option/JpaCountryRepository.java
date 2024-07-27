7bb1b4b818a0b705b8c79a06c9725f47bc3cb558
Q <99633210+Quathar@users.noreply.github.com>
2024-07-27 18:58:30 +0200

    feat() Create Option JPA repositories

---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/option/JpaCountryRepository.java
@@ -0,0 +1,15 @@
+package com.quathar.codebay.infra.jpa.repository.option;
+
+import com.quathar.codebay.infra.jpa.entity.option.CountryEntity;
+
+/**
+ * <h1>JPA (Java Persistence API) Country Repository</h1>
+ *
+ * @see JpaBaseOptionRepository
+ * @see CountryEntity
+ * @since 2023-05-13
+ * @version 1.0
+ * @author Q
+ */
+public interface JpaCountryRepository extends JpaBaseOptionRepository<CountryEntity> {
+}
---
+++ infrastructure/adapter-jpa/src/main/java/com/quathar/codebay/infra/jpa/repository/option/JpaRoadTypeRepository.java
@@ -0,0 +1,15 @@
+package com.quathar.codebay.infra.jpa.repository.option;
+
+import com.quathar.codebay.infra.jpa.entity.option.RoadTypeEntity;
+
+/**
+ * <h1>JPA (Java Persistence API) Road Type Repository</h1>
+ *
+ * @see JpaBaseOptionRepository
+ * @see RoadTypeEntity
+ * @since 2023-05-13
+ * @version 1.0
+ * @author Q
+ */
+public interface JpaRoadTypeRepository extends JpaBaseOptionRepository<RoadTypeEntity> {
+}
