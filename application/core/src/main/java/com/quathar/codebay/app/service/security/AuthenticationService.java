97f37d476deccbf5c006587c682f833438479723
Q <99633210+Quathar@users.noreply.github.com>
2024-07-28 22:06:40 +0200

    feat() Add refreshTokenUseCase in AuthenticationService

--- application/core/src/main/java/com/quathar/codebay/app/service/security/AuthenticationService.java
+++ application/core/src/main/java/com/quathar/codebay/app/service/security/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.quathar.codebay.app.service.security;
 
 import com.quathar.codebay.domain.port.in.security.AuthenticationUseCasePort;
+import com.quathar.codebay.domain.port.in.security.RefreshTokenUseCasePort;
 import com.quathar.codebay.domain.valueobject.security.TokenPair;
 
 import lombok.Builder;
@@ -16,15 +17,22 @@ import lombok.NonNull;
  */
 @Builder(builderMethodName = "setup", buildMethodName = "seal")
 public final class AuthenticationService
-       implements  AuthenticationUseCasePort {
+       implements  AuthenticationUseCasePort,
+                   RefreshTokenUseCasePort {
 
-    // <<-FIELD->>
+    // <<-FIELDS->>
     @NonNull private AuthenticationUseCasePort authenticationUseCase;
+    @NonNull private RefreshTokenUseCasePort   refreshTokenUseCase;
 
-    // <<-METHOD->>
+    // <<-METHODS->>
     @Override
     public TokenPair authenticate(String authenticationKey, String password) {
         return this.authenticationUseCase.authenticate(authenticationKey, password);
+    }
+
+    @Override
+    public TokenPair refresh(String token) {
+        return this.refreshTokenUseCase.refresh(token);
     }
 
 }
